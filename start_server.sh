da8daf07267acccd7cfdfa09c2de08fa955b188d
cedric <cedric.gautier.work@gmail.com>
2017-03-28 16:19:12 +0200

    Fixing activate type

--- start_server.sh
+++ start_server.sh
@@ -2,4 +2,4 @@
 mongod --dbpath="/root/work/mongodb" &
 export FLASK_APP=newsheadlinesfetcher.py
 export FLASK_DEBUG=1
-source dev/bin/activate.bat | python -m flask run 
+source dev/bin/activate | python -m flask run 
