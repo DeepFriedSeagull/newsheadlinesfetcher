091343372b82f87a9fc162c71c5e1a8d4f5147d2
Cédric GAUTIER <cedric.gautier.work@gmail.com>
2017-04-13 12:38:41 +0000

    Moving logs to proper folder and updating path in scripts

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 current_time=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
-ln -sf mongod.log.$current_time  mongod.log
-ln -sf flask.log.$current_time flask.log
-nohup mongod --config ./mongod.conf --logpath mongod.log.$current_time </dev/null >/dev/null 2>&1 &
+ln -sf mongod.log.$current_time logs/mongod.log
+ln -sf flask.log.$current_time logs/flask.log
+nohup mongod --config ./mongod.conf --logpath logs/mongod.log.$current_time </dev/null >/dev/null 2>&1 &
 export FLASK_APP=newsheadlinesfetcher
-# export FLASK_DEBUG=1
-source dev/bin/activate ; pip install -e . ; nohup flask run >flask.log.$current_time 2>&1 &
+#export FLASK_DEBUG=1
+source dev/bin/activate ; pip install -e . ; nohup flask run >logs/flask.log.$current_time 2>&1 &
